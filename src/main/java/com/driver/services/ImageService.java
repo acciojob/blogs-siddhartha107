44249427ac2780fcaaa0c9a96eff5a8bfd5fedbf
siddhartha107 <mishrasiddharth107@gmail.com>
2023-02-19 00:05:22 +0530

    sol1

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -5,8 +5,6 @@ import com.driver.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ImageService {
 
@@ -17,15 +15,45 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Blog blog = blogRepository2.findById(blogId).get();
+
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+
+        blog.getImageList().add(image);
+
+        blogRepository2.save(blog);
+
+        return image;
 
     }
 
     public void deleteImage(Integer id){
+        imageRepository2.deleteById(id);
 
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        int count = 0;
+        Image image = imageRepository2.findById(id).get();
+
+        if(image==null)return 0;
+
+
+        String imageDimension[]=image.getDimensions().split("X");
+        int imageLength = Integer.valueOf(imageDimension[0]),
+                imageBreadth =Integer.valueOf(imageDimension[1]);
+
+        String screenDimension[]=screenDimensions.split("X");
+        int screenLength = Integer.valueOf(screenDimension[0]),
+                screenBreadth = Integer.valueOf(screenDimension[1]);
+
+        count = (screenLength/imageLength) * (screenBreadth/imageBreadth);
+        
+        return count;
 
     }
 }
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -16,13 +16,30 @@ public class UserService {
     public User createUser(String username, String password){
 
 
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstName("test");
+        user.setLastName("test");
+
+        userRepository3.save(user);
+        return user;
+
+
     }
 
     public void deleteUser(int userId){
 
+        userRepository3.deleteById(userId);
+
     }
 
     public User updateUser(Integer id, String password){
+        User user = userRepository3.findById(id).get();
+        user.setPassword(password);
+
+        userRepository3.save(user);
+        return user;
 
     }
 }
